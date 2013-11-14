07e030eef9d8aefa72d26b3921a00a1598cd4ed3
William Hogman <me@whn.se>
2013-11-14 20:25:51 +0100

    Initial commit

---
+++ kistamarathon/src/main/java/se/whn/kistamarathon/App.java
@@ -0,0 +1,11 @@
+package se.whn.kistamarathon;
+
+/**
+ * Main class for running the kistamarathon app
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    }
+}
---
+++ kistamarathon/src/test/java/se/whn/kistamarathon/AppTest.java
@@ -0,0 +1,16 @@
+package se.whn.kistamarathon;
+
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ * Unit test for the app class.
+ */
+public class AppTest {
+
+}
